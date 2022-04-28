6291f8cbde4f59acfdd9c8b32e2682725fc4ec2c
SHAY2407 <yashwardhan2407@gmail.com>
2022-04-28 22:07:52 +0530

    indented the print statement

--- demo.py
+++ demo.py
@@ -4,16 +4,16 @@ This is a module listing the various domains under RUDRA and its activities
 
 """
 def RudraPrint():
-    print('RUDRA is the official team of SRM IST taking part in' 
-	  'University Rover Challenge organised by Mars Society since 2013.'
-	  'The annual competition is held at the Mars Desert Research Station (MDRS),' 
-	  'Martian Analog Site, near Hanksville, Utah, USA.'
-	  'The team has been successfully operational for the last 7 years '
-          'and has maintained its consistency in the competition.'
-	  'Our Team consists of 25-30 undergraduate students of every year '
-	  'from various engineering streams, working in different domains.' 
-	  'The fun part - R&D, is the soul and essence of Team Rudra. '
-	  'Brainstorming discussions are always welcome for giving birth to new ideas.')
+    print('\n\n\n\nRUDRA is the official team of SRM IST taking part in\n' 
+	  'University Rover Challenge organised by Mars Society since 2013.\n'
+	  'The annual competition is held at the Mars Desert Research Station (MDRS),\n' 
+	  'Martian Analog Site, near Hanksville, Utah, USA.\n'
+	  'The team has been successfully operational for the last 7 years \n'
+          'and has maintained its consistency in the competition.\n'
+	  'Our Team consists of 25-30 undergraduate students of every year \n'
+	  'from various engineering streams, working in different domains.\n' 
+	  'The fun part - R&D, is the soul and essence of Team Rudra.\n '
+	  'Brainstorming discussions are always welcome for giving birth to new ideas.\n')
 
 RudraPrint()
 
